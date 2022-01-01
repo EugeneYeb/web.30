2d0d23fe74b992493c981370155f4c431bde6134
eugene <info@eugeneyeboah.com>
2021-12-31 21:33:20 -0500

    verified icon imports

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import {HiMenuAlt4} from 'react-icons/hi';
-import {AiOutlineClose} from 'react-icns/ai';
+import {AiOutlineClose} from 'react-icons/ai';
 
 import logo from '../../images/logo.png';
 
 const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-center">
-      
+
     </nav>
   );
 }
