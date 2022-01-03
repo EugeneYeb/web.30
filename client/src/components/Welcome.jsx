2bbb779debfef4902472b46e4139f4039baff067
eugene <info@eugeneyeboah.com>
2022-01-02 23:12:36 -0500

    corrected return issue

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,6 +6,7 @@ import { Loader } from './';
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
 const Input = ({ placeholder, name, type, value, handleChange }) => {
+  return (
     <input
       placeholder={placeholder}
       type={type}
@@ -14,6 +15,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => {
       onChange={(e) => handleChange(e, name)}
       className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
     />
+  )
 };
 const Welcome = () => {
 
