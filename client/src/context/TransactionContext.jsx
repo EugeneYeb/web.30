f172257bb0dbee6265d5612fba9cc62166d32730
eugene <info@eugeneyeboah.com>
2022-01-05 19:59:44 -0500

    checkpoint

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -25,6 +25,9 @@ export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState('')
   const [formData, setFormData] = useState({ addressTo: '', amount: '', keyword: '', message: '' });
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [transactionCount, settransactionCount] = useState(localStorage.getItem('transactionCount'));
+
   const handleChange = (e, name) => {
     setFormData((prevState) => ({ ...prevState, [name]: e.targe.value }));
   }
@@ -79,7 +82,21 @@ export const TransactionProvider = ({ children }) => {
           gas: '0x5208', //21000 GWEI,
           value: parsedAmount._hex,
         }]
-      })
+      });
+
+      const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword);
+
+      setIsLoading(true);
+      console.log('Loading - ${transactionHash.hash}');
+      await transactionHash.wait();
+
+      setIsLoading(false);
+      console.log('Success - ${transactionHash.hash}');
+
+      const transactionCount = await transactionContract.getTransactionCount();
+
+      setTransactionCount(transactionCount.toNumber());
+      
     } catch (error) {
       console.log(error);
       throw new Error("No ethereum object.")
