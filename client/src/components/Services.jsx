add8e95b75575f5ced976828f999fff77d98a7f3
eugene <info@eugeneyeboah.com>
2022-01-05 20:11:36 -0500

    Services,jsx checkpoint

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,6 +1,29 @@
 const Services = () => {
+
+  import { BsShieldFillCheck } from 'react-icons/bs';
+  import { BiSearchAlt } from 'react-icons/bi';
+  import { RiHeath2Fill } from 'react-icons/ri';
+
+const ServiceCard = () => {
+  <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl"></div>
+}
   return (
-    <h1>Services</h1>
+
+    <div className="flex w-full justify-center items-center gradient-bg-services">
+      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+
+        <div className="flex-1 flex flex-col justify-start items-start">
+          <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">Services that we <br /> continue to Improve</h1>
+        </div>
+
+      </div>
+
+      <div className="flex-1 flex flex-col justify-start items-center">
+
+      </div>
+
+    </div>
+
   );
 }
 
