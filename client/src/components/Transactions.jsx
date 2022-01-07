4788b1d9ac2392df2b7eea0a023ce6fc6b9d708c
eugene <info@eugeneyeboah.com>
2022-01-06 23:11:35 -0500

    checkpoint

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,17 @@
+import React, { useContext } from 'react';
+import { transactionContext} from '../context/TransactionContext';
+import dummyData from '../utils/dummyData';
 const Transactions = () => {
   return (
-    <h1>Transactions</h1>
+    // https://youtu.be/Wn_Kb3MR_cU?t=8521
+    <div className="flex width-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+
+      {/* Latest Transactions */}
+      {/* Connect your account to see the latest transactions */}
+
+      </div>
+    </div>
   );
 }
 
