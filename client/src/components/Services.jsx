237e05222fb3be6dfd4d9f28300648352b079bf9
eugene <info@eugeneyeboah.com>
2022-01-09 16:25:57 -0500

    correct compilation issues

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -4,16 +4,19 @@ import { RiHeart2Fill } from 'react-icons/ri';
 
 const ServiceCard = ({ color, title, icon, subtitle }) => {
 
-  <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
-    <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
-      {icon}
-    </div>
+  return (
 
-    <div className="ml-5 flex flex-col flex-1">
-      <h3 className='mt-2 text-white text-lg'>{title}</h3>
-      <p className='mt-2 text-white text-sm md:w-9/12'>{subtitle}</p>
+    <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+      <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
+        {icon}
+      </div>
+
+      <div className="ml-5 flex flex-col flex-1">
+        <h3 className='mt-2 text-white text-lg'>{title}</h3>
+        <p className='mt-2 text-white text-sm md:w-9/12'>{subtitle}</p>
+      </div>
     </div>
-  </div>
+  );
 }
 
 const Services = () => {
