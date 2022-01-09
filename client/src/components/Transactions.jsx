e20d134c0e2e2ecdbbb557a766ed3837429fee55
eugene <info@eugeneyeboah.com>
2022-01-09 16:22:51 -0500

    fix compile issue

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { transactionContext } from '../context/TransactionContext';
+import { TransactionContext } from '../context/TransactionContext';
 import dummyData from '../utils/dummyData';
 import { shortenAddress } from '../utils/shortenAddress';
 import useFetch from '../hooks/useFetch';
